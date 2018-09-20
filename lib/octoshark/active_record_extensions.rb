3878fd5f1226a6164e33eca0af3085d5071145df
Marc Reynolds <mreynolds@doximity.com>
2018-09-20 17:19:43 -0600

    remove additional logging

--- lib/octoshark/active_record_extensions.rb
+++ lib/octoshark/active_record_extensions.rb
@@ -8,14 +8,6 @@ module Octoshark
 
   module ActiveRecordAbstractAdapter
     attr_accessor :connection_name, :database_name
-
-    def log(sql, name = "SQL", *other_args, &block)
-      if connection_name || database_name
-        name = "[Octoshark: #{[connection_name, database_name].compact.join(' ')}] #{name}"
-      end
-
-      super(sql, name, *other_args, &block)
-    end
   end
 end
 
